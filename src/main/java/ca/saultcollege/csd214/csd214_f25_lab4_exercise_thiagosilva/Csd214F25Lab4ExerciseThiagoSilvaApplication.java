93b414870b157fb8780585af793f2e02cd186a59
Thiago Silva <thiago.ervilha@gmail.com>
2025-11-18 16:27:05 -0500

    Part 1: Project Setup

---
+++ src/main/java/ca/saultcollege/csd214/csd214_f25_lab4_exercise_thiagosilva/Csd214F25Lab4ExerciseThiagoSilvaApplication.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.csd214.csd214_f25_lab4_exercise_thiagosilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csd214F25Lab4ExerciseThiagoSilvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csd214F25Lab4ExerciseThiagoSilvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/saultcollege/csd214/csd214_f25_lab4_exercise_thiagosilva/Csd214F25Lab4ExerciseThiagoSilvaApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.csd214.csd214_f25_lab4_exercise_thiagosilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csd214F25Lab4ExerciseThiagoSilvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
